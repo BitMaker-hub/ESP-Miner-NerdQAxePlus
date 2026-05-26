a41f9fdc0c757a8d9b8508e4790b0283f4734eaf
bitmaker <bitmaker.me@proton.me>
2026-05-26 03:07:41 +0200

    fix(web-ui): lower coreVoltage minimum validator from 1005mV to 800mV

--- main/http_server/axe-os/src/app/pages/edit/edit.component.ts
+++ main/http_server/axe-os/src/app/pages/edit/edit.component.ts
@@ -207,7 +207,7 @@ export class EditComponent implements OnInit {
           ssid: [info.ssid, [Validators.required]],
           wifiPass: ['*****'],
 
-          coreVoltage: [info.coreVoltage, [Validators.min(1005), Validators.max(1400), Validators.required]],
+          coreVoltage: [info.coreVoltage, [Validators.min(800), Validators.max(1400), Validators.required]],
           frequency: [info.frequency, [Validators.required]],
           jobInterval: [info.jobInterval, [Validators.required]],
           stratumDifficulty: [info.stratumDifficulty, [Validators.required, Validators.min(1)]],
